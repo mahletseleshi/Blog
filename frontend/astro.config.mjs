d20d6f544778cc325e617ce644fdcb3bcce90369
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-05-21 12:57:54 +0300

    fixed deployment issue

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,12 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
 
+// https://astro.build/config
 export default defineConfig({
   output: 'static',
   experimental: {
-    clientPrerender: true,
+    clientPrerender: true
   },
   integrations: [tailwind()],
+  adapter: vercel()
 });
